650c9b5860cb058cef754270a9e54698305a5ebc
chgwyellow <dinnis1107@gmail.com>
2025-12-10 22:48:58 +0800

    Finish data exploration.

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -9,12 +9,12 @@ def setup_logger(name: str = LOGGER_NAME) -> logging.Logger:
 
     logger = logging.getLogger(name)
     logger.setLevel(
-        logging, LOG_LEVEL.upper()
+        getattr(logging, LOG_LEVEL.upper())
     )  # DEBUG < INFO < WARNING < ERROR < CRITICAL
 
     # Console handler
     handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging, LOG_LEVEL.upper())
+    handler.setLevel(getattr(logging, LOG_LEVEL.upper()))
 
     # Format with emoji
     formatter = logging.Formatter("[%(levelname)s] %(message)s")
